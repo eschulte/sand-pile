4a58e1c972173bddc0049a15ffca3b8d3df2d732
Eric Schulte <eric.schulte@gmx.com>
2012-06-08 12:24:24 -0600

    code cleanup

--- sand-pile.lisp
+++ sand-pile.lisp
@@ -31,19 +31,6 @@
 ;;   pages={364--374}
 ;; }
 
-;; To run with graphics through gnuplot do the following:
-;;  1. rm -f /tmp/feedg
-;;  2. mkfifo /tmp/feedg
-;;  3. gnuplot < /tmp/feedg
-;;  4. evaluate the following
-;;     (with-open-file (out "/tmp/feedg" :direction :output :if-exists :append)
-;;       (run 1000 out))
-
-;;; Future Improvements:
-
-;; - track the size of each avalanche
-;; - track the average slope after every iteration
-
 ;;; Code:
 (require 'alexandria) (require 'metabang-bind)
 (defpackage #:sand-pile (:use :common-lisp :alexandria :metabang-bind))
@@ -60,41 +47,6 @@
 (defun set-z (coord new) (setf (apply #'aref *grid* coord) new))
 (defsetf z set-z)
 
-(defun init ()
-  "Randomly initialize the `*grid*'."
-  (mapc (lambda (coord) (setf (z coord) (random max-init)))
-        (cross (array-dimensions *grid*))))
-
-(defun update-rule (coord)
-  "Update a coordinate in the `*grid*'."
-  (flet ((slide (a b) (when (> (- (z a) (z b)) critical-slope)
-                        (incf (z b)) (decf (z a)) t)))
-    (mapc #'update-rule
-          (remove-if-not (lambda (neighbor)
-                           (when (or (slide coord neighbor)
-                                     (slide neighbor coord))
-                             neighbor))
-                         (sort (neighbors coord) #'<
-                               :key (lambda (it) (apply #'aref *grid* it)))))))
-
-(defun gnuplot (stream title)
-  (unless (= *dim* 2) (error "can only plot 2D grids"))
-  (format stream "set title '~a'~%" title)
-  (format stream "splot '-' matrix with pm3d notitle~%")
-  (format stream "~{~{~a~^ ~}~%~}e~%EOF~%"
-          (loop :for x :below (array-dimension *grid* 0)
-             :collect (loop :for y :below (array-dimension *grid* 1)
-                         :collect (aref *grid* x y)))))
-
-(defun run (steps stream)
-  (init)
-  (dotimes (n steps)
-    (let ((coord (random-elt (cross (array-dimensions *grid*)))))
-      (incf (z coord)) (update-rule coord))
-    (gnuplot stream n)))
-
-
-;;; Utility
 (defun range (n)
   (loop for i upto (1- n) collect i))
 
@@ -113,4 +65,37 @@
                                (cross (make-list *dim* :initial-element 3))
                                :test #'tree-equal)))))
 
-;;; sand-pile.lisp ends here
+(defun init ()
+  "Randomly initialize the `*grid*'."
+  (mapc (lambda (coord) (setf (z coord) (random max-init)))
+        (cross (array-dimensions *grid*))))
+
+(defun update (coord)
+  "Update a coordinate in the `*grid*'."
+  (flet ((slide (a b) (when (> (- (z a) (z b)) critical-slope)
+                        (incf (z b)) (decf (z a)) t)))
+    (mapc #'update
+          (remove-if-not (lambda (neighbor)
+                           (when (or (slide coord neighbor)
+                                     (slide neighbor coord))
+                             neighbor))
+                         (sort (neighbors coord) #'<
+                               :key (lambda (it) (apply #'aref *grid* it)))))))
+
+(defun gnuplot (stream title)
+  (unless (= *dim* 2) (error "can only plot 2D grids"))
+  (format stream "set term png~%")
+  (format stream "set output '~~/src/sand-pile/plots/~a.png'~%" title)
+  (format stream "set title '~a'~%" title)
+  (format stream "splot '-' matrix with pm3d notitle~%")
+  (format stream "~{~{~a~^ ~}~%~}e~%EOF~%"
+          (loop :for x :below (array-dimension *grid* 0)
+             :collect (loop :for y :below (array-dimension *grid* 1)
+                         :collect (aref *grid* x y)))))
+
+(defun run (steps stream)
+  (init)
+  (dotimes (n steps)
+    (let ((coord (random-elt (cross (array-dimensions *grid*)))))
+      (incf (z coord)) (update coord))
+    (gnuplot stream n)))
